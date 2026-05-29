989197112e90e4af4ff8878aec538cb8b94b4677
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-05-29 16:00:27 -0300

    Melhor

--- resource.h
+++ resource.h
@@ -6,7 +6,7 @@
 #define IDD_MY_DIALOG2 2
 #define IDD_MY_DIALOG3 3
 #define IDD_ABOUTBOX 100
-#define IDR_MAINFRAME 128
+#define IDR_MAINFRAME 101
 #define IDC_D2_MINIMOBOX 1000
 #define IDC_D2_MAXIMOBOX 1001
 #define IDC_D2_BUTTON 1002
@@ -16,7 +16,8 @@
 #define IDC_NAME3 1006
 #define IDC_EMAIL3 1007
 #define IDC_BUTTON3 1008
-#define IDC_HOME_KEYS 1016
+#define IDC_HOME_KEYS 1009
+#define IDC_D2_NUMEROS 1010
 
 // Next default values for new objects
 //
@@ -24,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE 311
 #define _APS_NEXT_COMMAND_VALUE 32771
-#define _APS_NEXT_CONTROL_VALUE 1018
+#define _APS_NEXT_CONTROL_VALUE 1011
 #define _APS_NEXT_SYMED_VALUE 310
 #endif
 #endif
